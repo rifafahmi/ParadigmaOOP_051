a6d1187c62c479eb0011f90637730953ae6a2a8d
rifa <rifafahmi21.com@gmai.com>
2023-05-22 11:36:12 +0700

    com1

--- ParadigmaOOP_051/ParadigmaOOP_051.cpp
+++ ParadigmaOOP_051/ParadigmaOOP_051.cpp
@@ -18,3 +18,19 @@ public:
 		return a + b;
 	}
 };
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+
+};
